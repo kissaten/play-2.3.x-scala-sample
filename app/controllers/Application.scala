9ffe36c86400c581716a154b806a359743de5e61
Joe Kutner <jpkutner@gmail.com>
2014-11-19 09:44:10 -0600

    added request id logging filter

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,26 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.Logger
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+object RequestIdLoggingFilter extends Filter {
+  def apply(nextFilter: (RequestHeader) => Future[Result])
+  (requestHeader: RequestHeader): Future[Result] = {
+    val startTime = System.currentTimeMillis
+    nextFilter(requestHeader).map { result =>
+      println(s"request_id=${requestHeader.headers.get("X-Request-ID")}")
+      Logger.info(s"request_id=${requestHeader.headers.get("X-Request-ID")}")
+      result
+    }
+  }
+}
+
+object Global extends WithFilters(RequestIdLoggingFilter) {
+
+}
+
 object Application extends Controller {
 
   def index = Action {
