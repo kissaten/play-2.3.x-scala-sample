09ce06c306349d3020bd766bdd1bea964343718f
Joe Kutner <jpkutner@gmail.com>
2015-02-24 17:49:59 -0600

    Added basic session example

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.cache.Cache
+import play.api.Play.current
 
 object Application extends Controller {
 
@@ -9,4 +11,23 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def login = Action {
+    val r = (new scala.util.Random).nextInt(100000)
+    Ok("Welcome!").withSession(
+      "connected" -> s"${r}@gmail.com"
+    );
+  }
+
+  def test = Action { request =>
+    val user = request.session.get("connected");
+    if(user != null) {
+      Ok("Hello " + user);
+    } else {
+      Unauthorized("Oops, you are not connected");
+    }
+  }
+
+  def logout = Action { request =>
+    Ok("Bye").withNewSession;
+  }
 }
